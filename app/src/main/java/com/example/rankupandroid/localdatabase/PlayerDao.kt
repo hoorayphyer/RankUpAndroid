03bf97209afb996401d0d7b8725dcb7b006ce439
hooray <hrfudan@gmail.com>
2021-07-13 00:10:31 -0400

    added local database

---
+++ app/src/main/java/com/example/rankupandroid/localdatabase/PlayerDao.kt
@@ -0,0 +1,17 @@
+package com.example.rankupandroid.localdatabase
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.rankupandroid.Player
+
+@Dao
+interface PlayerDao {
+    @Query("SELECT * FROM players_data_table")
+    fun getAllPlayers() : LiveData<List<Player>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun updateDatabase( asteroids : List<Player> )
+}
---
+++ app/src/main/java/com/example/rankupandroid/localdatabase/PlayerDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.rankupandroid.localdatabase
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.rankupandroid.Player
+
+@Database(entities = [Player::class], version = 1, exportSchema = false)
+abstract class PlayerDatabase : RoomDatabase() {
+    abstract val dao : PlayerDao
+
+    companion object {
+        // a singleton instance of the database
+        @Volatile
+        private var INSTANCE : PlayerDatabase? = null
+
+        fun getInstance(context: Context) : PlayerDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                // if there is not an existing instance of database, create one
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        PlayerDatabase::class.java,
+                        "rankup_player_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+
+
+}
