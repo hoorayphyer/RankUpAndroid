b6b767bd4aff7ed72ea67f04445d902f6cae25e9
hooray <hrfudan@gmail.com>
2021-09-25 18:08:16 -0400

    commented out the database for now, because I don't want to save players into it

--- app/src/main/java/com/example/rankupandroid/localdatabase/PlayerDao.kt
+++ app/src/main/java/com/example/rankupandroid/localdatabase/PlayerDao.kt
@@ -1,17 +1,19 @@
 package com.example.rankupandroid.localdatabase
 
-import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import com.example.rankupandroid.Player
+// TODO I don't want to save player into database
 
-@Dao
-interface PlayerDao {
-    @Query("SELECT * FROM players_data_table")
-    fun getAllPlayers() : LiveData<List<Player>>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun updateDatabase( asteroids : List<Player> )
-}
+//import androidx.lifecycle.LiveData
+//import androidx.room.Dao
+//import androidx.room.Insert
+//import androidx.room.OnConflictStrategy
+//import androidx.room.Query
+//import com.example.rankupandroid.Player
+//
+//@Dao
+//interface PlayerDao {
+//    @Query("SELECT * FROM players_data_table")
+//    fun getAllPlayers() : LiveData<List<Player>>
+//
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    fun updateDatabase( asteroids : List<Player> )
+//}
