c4a86667bb8e0cb8fce7094c32f5266cd6546b9f
hooray <hrfudan@gmail.com>
2021-10-10 21:25:37 -0400

    dealing cards to four players

--- app/src/main/java/com/example/rankupandroid/rankup/RankUpFragment.kt
+++ app/src/main/java/com/example/rankupandroid/rankup/RankUpFragment.kt
@@ -7,13 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.example.rankupandroid.R
 import com.example.rankupandroid.SharedViewModelSelectedPlayers
 import com.example.rankupandroid.databinding.FragmentRankUpBinding
 
+private var dealingBegin : Int = 0
+private var dealingEnd : Int = 4
+private var dealingIteration : Int = 0
+private val dealingDirection = arrayOf(R.id.dealtToBottom, R.id.dealtToRight, R.id.dealtToTop, R.id.dealtToLeft)
+
 class RankUpFragment : Fragment() {
     private lateinit var binding: FragmentRankUpBinding
+    private lateinit var motionLayout : MotionLayout
     private val viewModel: RankUpViewModel by viewModels()
     private val sharedModel: SharedViewModelSelectedPlayers by activityViewModels()
 
@@ -22,6 +30,8 @@ class RankUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRankUpBinding.inflate(layoutInflater, container, false)
+
+        motionLayout = binding.root
 
         binding.apply {
             sharedModel.myself.observe(viewLifecycleOwner, {
@@ -53,6 +63,16 @@ class RankUpFragment : Fragment() {
 
         binding.cardsRecyclerView.adapter = cardsAdapter
 
+
+        viewModel.gamePhase.observe(viewLifecycleOwner, {
+            setActionButton(it)
+        })
+
+        // setState sets the initial state of the motionLayout. Otherwise the first
+        // constraintSetStart of a <Transition> in the motion_scene.xml will serve the purpose
+        motionLayout.setState(R.id.atCenter, motionLayout.width, motionLayout.height)
+        motionLayout.addTransitionListener(CardDealingTransitionListener())
+
         return binding.root
     }
 
@@ -69,5 +89,71 @@ class RankUpFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         (requireActivity() as AppCompatActivity).supportActionBar?.show()
+    }
+
+    private fun setActionButton( gamePhase: GamePhase ) {
+        binding.rankupActionButton.apply{
+            when(gamePhase) {
+                GamePhase.END -> {
+                    visibility = View.VISIBLE
+                    text = getString(R.string.rankup_action_button_deal_str)
+                    setOnClickListener {
+                        viewModel.toNextPhase()
+                        dealCards()
+                    }
+                }
+                GamePhase.DEAL -> {
+                    visibility = View.INVISIBLE
+                    setOnClickListener { }
+                }
+                GamePhase.PLAY -> {
+                    visibility = View.VISIBLE
+                    text = getString(R.string.rankup_action_button_play_str)
+                    setOnClickListener {
+                        // TODO implement this
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun dealCards() {
+        dealingBegin = 0
+        dealingEnd = 8
+        dealingIteration = dealingBegin
+        val state = dealingDirection[dealingIteration % 4]
+        motionLayout.transitionToState(state, 1000)
+    }
+
+    private class CardDealingTransitionListener: MotionLayout.TransitionListener {
+        override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+        }
+
+        override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+            // setProgress doesn't invoke transition listener
+            motionLayout?.progress = 0.0f
+            ++dealingIteration
+            if (dealingIteration < dealingEnd) {
+                val state = dealingDirection[dealingIteration % 4]
+                motionLayout?.transitionToState(state, 1000)
+            }
+        }
+
+        override fun onTransitionChange(
+            motionLayout: MotionLayout?,
+            startId: Int,
+            endId: Int,
+            progress: Float
+        ) {
+        }
+
+        override fun onTransitionTrigger(
+            motionLayout: MotionLayout?,
+            triggerId: Int,
+            positive: Boolean,
+            progress: Float
+        ) {
+        }
     }
 }
--- app/src/main/java/com/example/rankupandroid/rankup/RankUpViewModel.kt
+++ app/src/main/java/com/example/rankupandroid/rankup/RankUpViewModel.kt
@@ -4,14 +4,29 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+enum class GamePhase {
+    END, DEAL, PLAY
+}
+
 class RankUpViewModel : ViewModel() {
     private val _cardsInHand: MutableLiveData<List<Card>> = MutableLiveData<List<Card>>()
     val cardsInHand: LiveData<List<Card>> = _cardsInHand
+
+    private val _gamePhase = MutableLiveData<GamePhase>(GamePhase.END)
+    val gamePhase : LiveData<GamePhase> = _gamePhase
 
     init {
         // TODO hard coded for tests
         _cardsInHand.value = (0..53).map {
             Card(it)
+        }
+    }
+
+    fun toNextPhase() {
+        _gamePhase.value = when(_gamePhase.value!!) {
+            GamePhase.END -> GamePhase.DEAL
+            GamePhase.DEAL -> GamePhase.PLAY
+            GamePhase.PLAY -> GamePhase.END
         }
     }
 }
